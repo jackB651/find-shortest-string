bb50093b037fae564d7d1c21418148277bd8ac5a
Jonathon Bialas <105708083+jackB651@users.noreply.github.com>
2023-03-14 23:59:17 -0500

    Solutions to find shortest string

---
+++ find_shortest_string.js
@@ -0,0 +1,35 @@
+function findShortestString(arr) {
+  // type your code here
+ let shortest = arr[0];
+
+  arr.forEach(string => {
+    if (string.length < shortest.length) {
+      shortest = string;
+    }
+  })
+
+  return shortest
+}
+
+if (require.main === module) {
+  // add your own tests in here
+  console.log("Expecting: 'a'");
+  console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
+
+  console.log("");
+
+  console.log("Expecting: 'hi'");
+  console.log("=>", findShortestString(['cat', 'hi', 'dog', 'an']));
+
+  console.log("");
+
+  console.log("Expecting: 'lily'");
+  console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
+
+  // BENCHMARK HERE
+}
+
+module.exports = findShortestString;
+
+// Please add your pseudocode to this file
+// And a written explanation of your solution
---
+++ find_shortest_string.rb
@@ -0,0 +1,32 @@
+def find_shortest_string(arr)
+  # type your code in here
+  shortest = arr[0]
+  arr.each do |t|
+    if t.length < shortest.length
+      shortest = t
+    end
+  end
+  return shortest
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts "Expecting: 'a'"
+  puts "=>", find_shortest_string(['aaa', 'a', 'bb', 'ccc'])
+
+  puts
+
+  puts "Expecting: 'hi'"
+  puts "=>", find_shortest_string(['cat', 'hi', 'dog', 'an'])
+
+  puts
+
+  puts "Expecting: 'lily'"
+  puts "=>", find_shortest_string(['flower', 'juniper', 'lily', 'dadelion'])
+
+  # Don't forget to add your own!
+
+  # BENCHMARK HERE
+end
+
+# Please add your pseudocode to this file
+# And a written explanation of your solution
